5cf637b2f12884dbf93c9c48e317010a117c4b14
GeniePrinie <geniexpn@gmail.com>
2022-08-26 13:19:18 +0200

    Try-catch handling, remove loading after api call

--- question-2.js
+++ question-2.js
@@ -5,34 +5,39 @@ const API_URL =
   key;
 
 const display = document.querySelector(".container");
+const circleContainer = document.querySelector(".circle");
 
 async function callRawgAPI() {
-  const response = await fetch(API_URL);
-  const obj_response = await response.json();
-  const results = obj_response.results;
+  try {
+    const response = await fetch(API_URL);
+    const objResponse = await response.json();
+    const results = objResponse.results;
 
-  let _name;
-  let rating;
-  let numOfTags;
+    let _name;
+    let rating;
+    let numOfTags;
 
-  for (let i = 0; i < 8; i++) {
-    const obj_result = results[i];
-    _name = obj_result.name;
-    rating = obj_result.rating;
-    numOfTags = obj_result.tags.length;
+    for (let i = 0; i < 8; i++) {
+      const result = results[i];
+      _name = result.name;
+      rating = result.rating;
+      numOfTags = result.tags.length;
 
-    console.log("OBJECT " + (i + 1));
-    console.log("Name = " + _name);
-    console.log("Rating = " + rating);
-    console.log("Number of tags = " + numOfTags);
-    console.log("--------------");
-
-    display.innerHTML += `<div class ="block">
+      display.innerHTML += `<div class ="block">
                             <h1>Object ${i + 1}</h1>
                             <p>Name: ${_name}</p>
                             <p>Rating: ${rating}</p>
                             <p>Number of tags: ${numOfTags}</p>
                           </div>`;
+    }
+  } catch (err) {
+    display.innerHTML = `<h1>${err}</h1>`;
   }
 }
-callRawgAPI();
+
+//simulating loading time
+
+setTimeout(function () {
+  callRawgAPI();
+  circleContainer.innerHTML = "";
+}, 2000);
